d130105a620d8b1b1f3b902550e5038160ba0fb1
John Bristowe <jbristowe@gmail.com>
2021-10-08 18:25:58 +1000

    chore: updated activation to remove command

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,6 @@ const extensionId = 'vscode-octopusdeploy';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log(`Extension "${extensionId}" activated.`);
-
-	let disposable = vscode.commands.registerCommand('vscode-octopusdeploy.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from vscode-octopusdeploy!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
