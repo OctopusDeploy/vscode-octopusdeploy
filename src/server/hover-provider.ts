845724214ce4332ec97a674d8542db5f398abae9
John Bristowe <jbristowe@gmail.com>
2021-10-20 11:51:05 +1000

    fix: removed hover provider

--- src/server/hover-provider.ts
+++
@@ -1,7 +0,0 @@
-import { CancellationToken, Hover, HoverProvider, TextDocument, Position } from 'vscode';
-
-class OclHoverProvider implements HoverProvider {
-    public provideHover(document: TextDocument, position: Position, token: CancellationToken): Thenable<Hover> {
-		return new Hover('I am a hover!');
-    }
-}
