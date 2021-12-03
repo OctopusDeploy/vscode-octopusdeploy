1b30a64ddc592a60c54efe5939549c650ea111b6
John Bristowe <john.bristowe@octopus.com>
2021-12-03 15:42:35 +1000

    refactor: type qualifications

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -1,35 +1,39 @@
-import { ExtensionContext, Uri } from 'vscode';
+import * as vscode from 'vscode';
 import { LanguageClientOptions } from 'vscode-languageclient';
 import { LanguageClient } from 'vscode-languageclient/browser';
-import { EXTENSION_ID, LANGUAGE_CLIENT_NAME, OCL_LANGUAGE_ID, SETUP_API_KEY_CMD } from './constants';
-import { OclOutline } from './services/ocl-outline-provider';
+import { EXTENSION_ACTIVATED, EXTENSION_ID, EXTENSION_SERVER_IS_READY, LANGUAGE_CLIENT_NAME, OCL_LANGUAGE_ID } from './constants';
+import { registerAuthSetup, registerOclOutlineProvider, registerOctopusExplorerProvider } from './events';
+import { OctopusDeploySettings } from './settings';
 
-export function activate(context: ExtensionContext) {
-	console.log(`${EXTENSION_ID} activated.`);
+export async function activate(context: vscode.ExtensionContext) {
+    console.log(EXTENSION_ACTIVATED);
 
-	new OclOutline();
+    OctopusDeploySettings.init(context);
+    registerAuthSetup(context);
 
-	const documentSelector = [{ language: OCL_LANGUAGE_ID }];
-	const clientOptions: LanguageClientOptions = {
-		documentSelector,
-		synchronize: {},
-		initializationOptions: {}
-	};
+    await registerOclOutlineProvider(context);
+    await registerOctopusExplorerProvider(context);
 
-	const client = createWorkerLanguageClient(context, clientOptions);
-	const disposable = client.start();
-	context.subscriptions.push(disposable);
+    const documentSelector = [{ language: OCL_LANGUAGE_ID }];
+    const clientOptions: LanguageClientOptions = {
+        documentSelector,
+        synchronize: {},
+        initializationOptions: {},
+    };
 
-	client.onReady().then(() => {
-		console.log(`${EXTENSION_ID} server is ready`);
-	});
+    const client = createWorkerLanguageClient(context, clientOptions);
+    context.subscriptions.push(client.start());
+
+    client.onReady().then(() => {
+        console.log(EXTENSION_SERVER_IS_READY);
+    });
 }
 
-function createWorkerLanguageClient(context: ExtensionContext, clientOptions: LanguageClientOptions) {
-	// create worker; implements the language server
-	const serverMain = Uri.joinPath(context.extensionUri, 'dist/server/browserServerMain.js');
-	const worker = new Worker(serverMain.toString());
+function createWorkerLanguageClient(context: vscode.ExtensionContext, clientOptions: LanguageClientOptions) {
+    // create worker; implements the language server
+    const serverMain = vscode.Uri.joinPath(context.extensionUri, 'dist/server/browserServerMain.js');
+    const worker = new Worker(serverMain.toString());
 
-	// create the client to communicate with language server running in worker
-	return new LanguageClient(EXTENSION_ID, LANGUAGE_CLIENT_NAME, clientOptions, worker);
+    // create the client to communicate with language server running in worker
+    return new LanguageClient(EXTENSION_ID, LANGUAGE_CLIENT_NAME, clientOptions, worker);
 }
